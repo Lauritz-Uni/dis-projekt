fd850f45b081fdbca4ba44ea9e440b90dffb4152
ifndude <ifnande@gmail.com>
2025-05-29 15:54:20 +0200

    deleting changes because laurtiz made a better one

--- project/import_data.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from website.models import Movie, Review
-from website import create_app, db
-
-load_dotenv() # Load env variables
-DATABASE_URL = os.getenv("DATABASE_URL")
