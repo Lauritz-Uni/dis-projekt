3701638396848e5488d4a84ce44c6140ee2d6f2d
ifndude <ifnande@gmail.com>
2025-05-29 14:10:18 +0200

    Set up of database and envioronment configs

--- project/website/__init__.py
+++ project/website/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from dotenv import load_dotenv
+import os 
+
+db = SQLAlchemy()
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your mother is extremely fat'
+
+    app.config['SECRET_KEY'] = os.getenv('SECRET-KEY', 'fat mom')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    
+    db.init_app(app)
 
     from .views import views
     from. auth import auth
