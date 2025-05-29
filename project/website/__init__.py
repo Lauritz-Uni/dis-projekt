eb83933b16300dbbc22c9b0a4e8ba43f1a82348a
ifndude <ifnande@gmail.com>
2025-05-29 15:54:20 +0200

    deleting changes because laurtiz made a better one

--- project/website/__init__.py
+++ project/website/__init__.py
@@ -1,18 +1,9 @@
 from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy
-from os import path
-from dotenv import load_dotenv
-import os 
-
-db = SQLAlchemy()
-load_dotenv()
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = os.getenv('SECRET-KEY', 'fat mom')
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     
     db.init_app(app)
 
