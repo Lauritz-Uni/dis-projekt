12344482d0992fe7985c0aadb22fddcab8502f3b
Lauritz-Uni <fqm630@alumni.ku.dk>
2025-06-01 17:32:08 +0200

    Added search

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
-#store standard standard root to where users can access like homepage. 
-import random
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, request
 from .models import Movie
 from . import db
 from  sqlalchemy.sql.expression import func, select
 import movieposters as mp
+from sqlalchemy import text
+from markupsafe import escape
 
 views = Blueprint('views', __name__)
 
@@ -29,3 +29,28 @@ def get_poster(movie_title):
         return jsonify({'poster': poster_url})
     except Exception as e:
         return jsonify({'poster': None})
+
+@views.route('/search')
+@views.route('/search')
+def search():
+    query = request.args.get('q', '')
+    sanitized_query = escape(query)  # for safe HTML rendering
+
+    if not query:
+        return render_template("search_results.html", movies=[], query=sanitized_query)
+
+    sql = text("""
+        SELECT * FROM movie
+        WHERE title ILIKE :search_term
+    """)
+    search_term = f"%{query}%"
+    results = db.session.execute(sql, {'search_term': search_term}).fetchall()
+
+    # Convert results to dicts (since raw SQL gives RowProxy objects)
+    movies = []
+    for row in results:
+        movie = dict(row._mapping)  # for SQLAlchemy 1.4+ use row._mapping
+        movies.append(movie)
+
+    user = type('user', (object,), {'is_authenticated': False})()  # fake user
+    return render_template("search_results.html", movies=movies, query=sanitized_query, user=user)
